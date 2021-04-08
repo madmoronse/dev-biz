cb289864a5195588b573c64bdcf95f2dddc1c746
madmoronse <smolyaninov.se@yandex.ru>
2021-04-08 15:34:56 +0700

    change config cake

--- sites/dev-cake/config.php
+++ sites/dev-cake/config.php
@@ -7,7 +7,7 @@ define(DIR_CONTROLLER, 'controllers/');
 define(DIR_MODEL, 'models/');
 
 
-define(DB_HOST, 'localhost');
+define(DB_HOST, 'mysql');
 define(DB_NAME, 'cakes');
 define(DB_USER, 'root');
-define(DB_PASS, '');
+define(DB_PASS, 'devpass');
