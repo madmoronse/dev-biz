62c8763733796bb0d03dd0da20d4a05e946fe6c8
madmoronse <smolyaninov.se@yandex.ru>
2021-04-08 14:15:13 +0700

    docker create

---
+++ sites/dev-biz/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://test-oc/admin/');
+define('HTTP_CATALOG', 'http://test-oc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://test-oc/admin/');
+define('HTTPS_CATALOG', 'http://test-oc/');
+
+// DIR
+define('DIR_APPLICATION', '../admin/');
+define('DIR_SYSTEM', '../system/');
+define('DIR_DATABASE', '../system/database/');
+define('DIR_LANGUAGE', '../admin/language/');
+define('DIR_TEMPLATE', '../admin/view/template/');
+define('DIR_CONFIG', '../system/config/');
+define('DIR_IMAGE', '../image/');
+define('DIR_CACHE', '../system/cache/');
+define('DIR_DOWNLOAD', '../download/');
+define('DIR_LOGS', '../system/logs/');
+define('DIR_CATALOG', '../catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'outmax');
+define('DB_PREFIX', 'oc_');
+?>
